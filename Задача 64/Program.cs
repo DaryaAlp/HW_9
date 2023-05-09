2432bab203da0ad6caad50b3c8eaf97ec427bfa3
Traveller <you@example.com>
2023-05-09 16:20:05 +0300

    Добавили решение задач 64, 66, 68

---
+++ Задача 64/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите N: ");
+int n=int.Parse(ReadLine());
+
+void NumberCounter (int n)
+{
+    if (n == 0) return;
+Write("{0,4}", n);
+NumberCounter (n - 1);
+}
+
+NumberCounter(n);
---
+++ Задача 66/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных 
+// элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите число M: ");
+int m = int.Parse(ReadLine());
+Console.Write("Введите число N: ");
+int n = int.Parse(ReadLine());
+
+
+void SumNumber(int m, int n, int sum)
+{
+    if (m > n) 
+    {
+        WriteLine($"Сумма натуральных элементов от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (m++);
+    SumNumber(m, n, sum);
+}
+
+SumNumber(m, n, 0);
